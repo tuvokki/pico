ce9fdc92803863aa13e02343c846754cc10cfef2
Wouter Roosendaal <wouter.roosendaal@planonsoftware.com>
2018-08-30 11:45:47 +0200

    Add RPC to haha's jokes

--- haha/rpc_methods.py
+++ haha/rpc_methods.py
@@ -1,6 +1,16 @@
 from modernrpc.core import rpc_method
+import json
+from .models import Joke
 
 
 @rpc_method
 def add(a, b):
     return a + b
+
+
+@rpc_method
+def last():
+    last_jokes = Joke.objects.order_by('-pub_date')[:5]
+    # return json.dumps(last_jokes, indent=4, sort_keys=True, default=str)
+    result_list = list(last_jokes.values('title', 'body', 'punchline'))  # 'pub_date' is not serializable
+    return json.dumps(result_list)
