562b9e4de6bb663e6f9ff8c8691f4b037de37cd2
Wouter <tuvokki@gmail.com>
2018-09-09 10:08:52 +0200

    Use dev version of import/export

--- mopo/admin.py
+++ mopo/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from .models import Quote
 
-admin.site.register(Quote)
+
+class QuoteResource(resources.ModelResource):
+
+    class Meta:
+        model = Quote
+
+
+class QuoteAdmin(ImportExportModelAdmin):
+    resource_class = QuoteResource
+
+
+admin.site.register(Quote, QuoteAdmin)
