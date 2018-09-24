e731679821561c6647716ce767612c6907a40978
Wouter Roosendaal <wouter@yellenge.nl>
2018-09-24 10:56:45 +0200

    Add a random jokes method

--- haha/rpc_methods.py
+++ haha/rpc_methods.py
@@ -1,7 +1,11 @@
 from modernrpc.core import rpc_method
-import json
-from .models import Joke
+from django.db.models import Max
 from datetime import date
+import random
+import json
+
+from .models import Joke
+
 
 @rpc_method
 def add(title, body, punchline):
@@ -14,6 +18,14 @@ def add(title, body, punchline):
 @rpc_method
 def last(number=5):
     last_jokes = Joke.objects.order_by('-pub_date')[:number]
-    # return json.dumps(last_jokes, indent=4, sort_keys=True, default=str)
     result_list = list(last_jokes.values('title', 'body', 'punchline'))  # 'pub_date' is not serializable
+    return json.dumps(result_list)
+
+
+@rpc_method(name='random')
+def random_list(number=5):
+    # Note: order_by('?') queries may be expensive and slow, depending on the database backend
+    random_jokes = Joke.objects.all().order_by('?')[:number]
+    result_list = list(random_jokes.values('title', 'body', 'punchline'))
+
     return json.dumps(result_list)
